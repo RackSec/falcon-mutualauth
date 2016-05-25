5bd327c5cae1d6dca74f2fdf8056083366fcde09
Elana Hashman <elana.hashman@rackspace.com>
2016-05-25 16:42:17 +0000

    Package this a bit more nicely

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mutualauth',
+    name='falcon-mutualauth',
     version='0.1',
     description='Mutual TLS authentication for Falcon',
     author='Rackspace',
     author_email='',
     url='',
     license='Apache 2.0',
-    packages=[''],
+    packages=find_packages(),
     install_requires=[
         'falcon',
         'structlog',
