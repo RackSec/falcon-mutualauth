244236e032987776212c61901b61071ae5973ed5
Elana Hashman <elana.hashman@rackspace.com>
2016-06-10 15:55:51 +0000

    Bump version to 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='falcon-mutualauth',
-    version='0.3',
+    version='0.4',
     description='Mutual TLS authentication for Falcon',
     author='Rackspace',
     author_email='',
